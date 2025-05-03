0d344848d9e03f279002f62c09d729339fab639b
clonetrooper134579474 <138741803+clonetrooper134579474@users.noreply.github.com>
2025-05-03 18:07:38 -0400

    Add files via upload

---
+++ cookie/ajax.js
@@ -0,0 +1,40 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+function ajax(url,callback){
+	var ajaxRequest;
+	try{ajaxRequest = new XMLHttpRequest();} catch (e){try{ajaxRequest=new ActiveXObject('Msxml2.XMLHTTP');} catch (e) {try{ajaxRequest=new ActiveXObject('Microsoft.XMLHTTP');} catch (e){alert("Something broke!");return false;}}}
+	if (callback){ajaxRequest.onreadystatechange=function(){if(ajaxRequest.readyState==4){callback(ajaxRequest.responseText);}}}
+	ajaxRequest.open('GET',url+'&nocache='+(new Date().getTime()),true);ajaxRequest.send(null);
+}
+
+}
+/*
+     FILE ARCHIVED ON 13:49:13 Aug 25, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 20:10:11 May 02, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1.975
+  exclusion.robots: 0.038
+  exclusion.robots.policy: 0.024
+  esindex: 0.016
+  cdx.remote: 41.216
+  LoadShardBlock: 178.019 (3)
+  PetaboxLoader3.datanode: 210.693 (4)
+  load_resource: 131.274
+  PetaboxLoader3.resolve: 70.407
+*/
