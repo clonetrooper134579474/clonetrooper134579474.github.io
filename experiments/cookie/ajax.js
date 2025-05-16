f0ac25aa4a9d53dd6e31e35aee9a79c245092dec
clonetrooper134579474 <138741803+clonetrooper134579474@users.noreply.github.com>
2025-05-16 17:41:48 -0400

    Add files via upload

---
+++ experiments/cookie/ajax.js
@@ -0,0 +1,40 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+function ajax(url,callback){
+	var ajaxRequest;
+	try{ajaxRequest = new XMLHttpRequest();} catch (e){try{ajaxRequest=new ActiveXObject('Msxml2.XMLHTTP');} catch (e) {try{ajaxRequest=new ActiveXObject('Microsoft.XMLHTTP');} catch (e){alert("Something broke!");return false;}}}
+	if (callback){ajaxRequest.onreadystatechange=function(){if(ajaxRequest.readyState==4){callback(ajaxRequest.responseText);}}}
+	ajaxRequest.open('GET',url+'&nocache='+(new Date().getTime()),true);ajaxRequest.send(null);
+}
+
+}
+/*
+     FILE ARCHIVED ON 04:28:05 Aug 20, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 21:30:54 May 14, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.56
+  exclusion.robots: 0.021
+  exclusion.robots.policy: 0.008
+  esindex: 0.013
+  cdx.remote: 6.462
+  LoadShardBlock: 74.51 (3)
+  PetaboxLoader3.datanode: 79.589 (4)
+  PetaboxLoader3.resolve: 102.769 (2)
+  load_resource: 113.94
+*/
