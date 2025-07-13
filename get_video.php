00252acca5bb3eed049fdef38a428459c8a7c616
clonetrooper134579474 <138741803+clonetrooper134579474@users.noreply.github.com>
2025-07-13 14:30:40 -0400

    Update get_video.php

--- get_video.php
+++ get_video.php
@@ -1,5 +1,5 @@
 <?php
 header("Content-Type: video/flv");
-header('Content-Length: ' . filesize('video.flv'));
-readfile('video.flv');
+header('Content-Length: ' . filesize('get_video.flv'));
+readfile('get_video.flv');
 ?>
